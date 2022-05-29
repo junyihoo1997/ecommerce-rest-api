44c514e47a29f0d10063987c92af05fbbbbc49c5
junyihoo1997 <junyi.hoo@newsoft.com.my>
2022-05-29 22:24:55 +0800

    Modified typescript lambda to javascript

--- infrastructure/EcommerceRestApiStack.ts
+++ infrastructure/EcommerceRestApiStack.ts
@@ -9,6 +9,7 @@ import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 export class EcommerceRestApiStack extends Stack{
 
     private api = new RestApi(this, 'EcommerceApi');
+
     private productTable = new GenericTable(
         'ProductTable',
         'productId',
@@ -18,15 +19,16 @@ export class EcommerceRestApiStack extends Stack{
     constructor(scope: Construct, id: string, props: StackProps){
         super(scope, id, props)
 
-        // const productLambda = new LambdaFunction(this, 'productLambda',{
-        //     runtime: Runtime.NODEJS_16_X,
-        //     code: Code.fromAsset(join(__dirname,'..','services','ProductTable')),
-        //     handler: 'product.main'
-        // });
+        const productLambda = new LambdaFunction(this, 'productLambda',{
+            runtime: Runtime.NODEJS_16_X,
+            code: Code.fromAsset(join(__dirname,'..','services','ProductTable')),
+            handler: 'product.handler'
+        });
 
-        const productLambda = new NodejsFunction(this, 'productLambda', {
-            entry: (join(__dirname,'..','services','ProductTable', 'product.ts'))
-        })
+        // const productLambda = new NodejsFunction(this, 'productLambda', {
+        //     entry: (join(__dirname,'..','services','ProductTable', 'product.js')),
+        //     handler: 'handler'
+        // })
 
         const productLambdaIntegration = new LambdaIntegration(productLambda);
         const productLambdaResource = this.api.root.addResource('product');
--- services/ProductTable/product.ts
+++
@@ -1,6 +0,0 @@
-async function handler(event: any, context:any) {
-    return {
-        statusCode: 200,
-        body: 'Hello World from aws cdk'
-    }
-}
